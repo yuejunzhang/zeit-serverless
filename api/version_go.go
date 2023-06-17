92699d58ff7897401c1dde3b209b0e6187a7ab62
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-06-17 16:47:54 +0800

    Update version_go.go

--- api/version_go.go
+++ api/version_go.go
@@ -2,7 +2,7 @@ package handler
  
 
 import (
-	"io"
+	
 	"log"
 	"os"
 	"fmt"
@@ -10,8 +10,7 @@ import (
 	http "github.com/bogdanfinn/fhttp"
 	tls_client "github.com/bogdanfinn/tls-client"
 
-	"github.com/fvbock/endless"
-	"github.com/gin-gonic/gin"
+
 )
 
 var (
@@ -24,9 +23,10 @@ var (
 	}
 	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
 	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-	http_proxy = os.Getenv("http_proxy")
+	
 )
 func Handler(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+	
 }
 
