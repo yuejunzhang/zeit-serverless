ddbbcdddcc01a42ae5f97cc6f85e70b9b2cd2f31
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-06-17 16:23:23 +0800

    Update version_go.go

--- api/version_go.go
+++ api/version_go.go
@@ -25,5 +25,7 @@ var (
 	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
 	http_proxy = os.Getenv("http_proxy")
 )
-
+func Handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
 
