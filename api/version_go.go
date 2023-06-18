cd3935a886fdf3ee26370c54e5706aaef95528a2
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-06-18 13:46:37 +0800

    Update version_go.go

--- api/version_go.go
+++ api/version_go.go
@@ -70,17 +70,18 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<pre>%v</pre>", request)
 		return
 	}
+		fmt.Fprintf(w, "<h1>ok creating request: %v</h1>", err)
+		fmt.Fprintf(w, "<h2>Request:</h2>")
+		fmt.Fprintf(w, "<pre>%v</pre>", request)
+		fmt.Fprintf(w, "<h1>ok creating response: %v</h1>")
+		fmt.Fprintf(w, "<h2>response:</h2>")
+		fmt.Fprintf(w, "<pre>%v</pre>", response)
 	defer response.Body.Close()
 	w.Header().Set("Content-Type", response.Header.Get("Content-Type"))
 
 	// Get status code
 	w.WriteHeader(response.StatusCode)
-		fmt.Fprintf(w, "<h1>ok creating request: %v</h1>", err)
-		fmt.Fprintf(w, "<h2>Request:</h2>")
-		fmt.Fprintf(w, "<pre>%v</pre>", request)
-		fmt.Fprintf(w, "<h1>ok creating response: %v</h1>", err)
-		fmt.Fprintf(w, "<h2>response:</h2>")
-		fmt.Fprintf(w, "<pre>%v</pre>", response)
+
 
 	buf := make([]byte, 4096)
 	for {
