6dfec05e80851389c5d89ee5e97037a59c525fa0
yuejunzhang <58799137+yuejunzhang@users.noreply.github.com>
2023-06-19 10:22:02 +0800

    Update version_go.go

--- api/version_go.go
+++ api/version_go.go
@@ -9,31 +9,30 @@ import (
 	"log"
 	"net/http"
 	fhttp "github.com/bogdanfinn/fhttp"
-	tls_client "github.com/bogdanfinn/tls-client"
+	// tls_client "github.com/bogdanfinn/tls-client"
 	
 	"bufio"
 	"encoding/json"
 	"./internal/chatgpt"
 	"./internal/tokens"
-	typings "./internal/typings"
 	"./internal/typings/responses"
-	//"./chatgpt"
+	typings "./internal/typings"
 )
 
-var (
-	jar     = tls_client.NewCookieJar()
-	options = []tls_client.HttpClientOption{
-		tls_client.WithTimeoutSeconds(360),
-		tls_client.WithClientProfile(tls_client.Chrome_112),
-		//tls_client.WithClientProfile(tls_client.Firefox_110),
-		tls_client.WithNotFollowRedirects(),
-		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
-		tls_client.WithInsecureSkipVerify(),
-	}
-	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
-	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+// var (
+// 	jar     = tls_client.NewCookieJar()
+// 	options = []tls_client.HttpClientOption{
+// 		tls_client.WithTimeoutSeconds(360),
+// 		tls_client.WithClientProfile(tls_client.Chrome_112),
+// 		//tls_client.WithClientProfile(tls_client.Firefox_110),
+// 		tls_client.WithNotFollowRedirects(),
+// 		tls_client.WithCookieJar(jar), // create cookieJar instance and pass it as argument
+// 		tls_client.WithInsecureSkipVerify(),
+// 	}
+// 	client, _  = tls_client.NewHttpClient(tls_client.NewNoopLogger(), options...)
+// 	user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
 	
-)
+// )
 func Handler(w http.ResponseWriter, r *http.Request) {//r下游请求,w下游响应
 	var url string
 	var err error
